0ab6fe10ffe454d08ea5d65acf7e3eaffd892e20
Minahil Imtiaz <minahil.imtiaz@tintash.com>
2023-11-22 23:37:24 +0200

    doc: set up swagger ui
    doc: add endpoints documentation in swagger.json

--- src/api/routes/v1/index.ts
+++ src/api/routes/v1/index.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
-import validRoutes from './validRouter';
+import swaggerUi from 'swagger-ui-express';
+import swaggerSpec from '../../../docs/swagger.json';
 import appointmentRoutes from './appointmentRouter';
 
 const router = Router();
@@ -10,13 +11,13 @@ const router = Router();
 router.get('/status', (req, res) => res.send('OK'));
 
 /**
- * ALL v1/valid
- */
-router.use('/valid', validRoutes);
-
-/**
  * ALL v1/appointment
  */
 router.use('/appointment', appointmentRoutes);
+
+/**
+ * GET v1/api-docs
+ */
+router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 export default router;
